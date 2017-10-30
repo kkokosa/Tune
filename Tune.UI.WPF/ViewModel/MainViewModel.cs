d45371555c3fc33437256cfdb121d2d17b1c7e2b
Konrad Kokosa <kkokosa@f28-studio.com>
2017-10-30 22:12:36 +0100

    WPF MVVM binding improvements

--- Tune.UI.WPF/ViewModel/MainViewModel.cs
+++ Tune.UI.WPF/ViewModel/MainViewModel.cs
@@ -3,6 +3,7 @@ using GalaSoft.MvvmLight.Command;
 using System;
 using System.Reflection;
 using System.Threading;
+using System.Threading.Tasks;
 using System.Windows.Input;
 using System.Windows.Threading;
 using Tune.Core;
@@ -14,15 +15,21 @@ namespace Tune.UI.WPF.ViewModel
         private DiagnosticEngine engine;
         private readonly Assembly mainAssembly;
         private string scriptText;
+        private string scriptArgument;
         private string logText;
         private string ilText;
         private string asmText;
+        private DiagnosticAssemblyMode assemblyMode;
+        private DiagnosticAssembyPlatform assembyPlatform;
+        private MainViewModelState state;
 
         /// <summary>
         /// Initializes a new instance of the MainViewModel class.
         /// </summary>
         public MainViewModel()
         {
+            this.scriptArgument = "<Argument>";
+            this.state = MainViewModelState.Idle;
             this.mainAssembly = Assembly.GetEntryAssembly();
             this.RunScriptCommand = new RelayCommand(RunScript, CanRunScript);
             this.engine = new DiagnosticEngine();
@@ -34,7 +41,7 @@ namespace Tune.UI.WPF.ViewModel
             get
             {
                 Version version = this.mainAssembly.GetName().Version;
-                var titleAttribute = (AssemblyTitleAttribute) Attribute.GetCustomAttribute(this.mainAssembly,
+                var titleAttribute = (AssemblyTitleAttribute)Attribute.GetCustomAttribute(this.mainAssembly,
                     typeof(AssemblyTitleAttribute));
                 return $"{titleAttribute.Title} {version}";
             }
@@ -46,10 +53,22 @@ namespace Tune.UI.WPF.ViewModel
             set { this.scriptText = value; RaisePropertyChanged(nameof(ScriptText)); this.RunScriptCommand.RaiseCanExecuteChanged(); }
         }
 
+        public string ScriptArgument
+        {
+            get { return this.scriptArgument; }
+            set { this.scriptArgument = value; RaisePropertyChanged(nameof(ScriptArgument)); }
+        }
+
         public string LogText
         {
             get { return this.logText; }
             private set { this.logText = value; RaisePropertyChanged(nameof(LogText)); }
+        }
+
+        public MainViewModelState State
+        {
+            get { return this.state; }
+            set { this.state = value; RaisePropertyChanged(nameof(State)); this.RunScriptCommand.RaiseCanExecuteChanged(); }
         }
 
         public string IlText
@@ -63,27 +82,39 @@ namespace Tune.UI.WPF.ViewModel
             private set { this.asmText = value; RaisePropertyChanged(nameof(AsmText)); }
         }
 
+        public DiagnosticAssemblyMode AssemblyMode
+        {
+            get { return this.assemblyMode; }
+            private set { this.assemblyMode = value; RaisePropertyChanged(nameof(AssemblyMode)); }
+        }
+        public DiagnosticAssembyPlatform AssemblyPlatform
+        {
+            get { return this.assembyPlatform; }
+            private set { this.assembyPlatform = value; RaisePropertyChanged(nameof(AssemblyPlatform)); }
+        }
+
         public RelayCommand RunScriptCommand { get; private set; }
 
-        private void RunScript()
+        private async void RunScript()
         {
-            ThreadPool.QueueUserWorkItem((obj) => RunAsync(obj), Tuple.Create(ScriptText, "<Argument>"));
+            UpdateLog("Running started.");
+            this.State = MainViewModelState.Running;
+            var cancellationTokenSource = new CancellationTokenSource();
+            var progressReport = new Progress<string>((status) => UpdateLog($"  Running..."));
+            var token = cancellationTokenSource.Token;
+            var result = await Task.Run(() => RunAsync(this.scriptText, this.scriptArgument, this.assemblyMode, this.assembyPlatform),
+                token);
+            this.State = MainViewModelState.Idle;
+            UpdateLog($"Running ended with success {result}");
         }
 
         private bool CanRunScript()
         {
-            return !string.IsNullOrWhiteSpace(this.ScriptText);
+            return !string.IsNullOrWhiteSpace(this.ScriptText) && this.state != MainViewModelState.Running;
         }
 
-        private void RunAsync(object parameters)
+        private async Task<bool> RunAsync(string script, string argument, DiagnosticAssemblyMode level, DiagnosticAssembyPlatform platform)
         {
-            var tuple = parameters as Tuple<string, string>;
-            var script = tuple.Item1;
-            var argument = tuple.Item2;
-            //var level = cbMode.SelectedItem.ToString() == "Release" ? DiagnosticAssemblyMode.Release : DiagnosticAssemblyMode.Debug;
-            var level = DiagnosticAssemblyMode.Release;
-            var platform = DiagnosticAssembyPlatform.x64;
-
             try
             {
                 var assembly = engine.Compile(script, level, platform);
@@ -93,10 +124,12 @@ namespace Tune.UI.WPF.ViewModel
                 string asmText = assembly.DumpASM();
                 UpdateASM(asmText);
                 UpdateLog("Script processing ended.");
+                return true;
             }
             catch (Exception ex)
             {
                 UpdateLog(ex.ToString());
+                return false;
             }
 
         }
@@ -134,5 +167,11 @@ namespace Tune.UI.WPF.ViewModel
                 : $"{str}{Environment.NewLine}";
             LogText += log;
         }
+    }
+
+    public enum MainViewModelState
+    {
+        Idle,
+        Running
     }
 }
